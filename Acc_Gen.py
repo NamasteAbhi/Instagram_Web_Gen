b8edb81fe01edb9fedf6abd61f5309d5cfd63cd4
Namaste Coder <95855028+NamasteAbhi@users.noreply.github.com>
2024-12-31 22:23:15 +0530

    Update Acc_Gen.py

--- Acc_Gen.py
+++ Acc_Gen.py
@@ -12,19 +12,17 @@ requests.impersonate='chrome110'
 def get_headers(Country,Language):
     while True:
         try:
-
             an_agent=f'Mozilla/5.0 (Linux; Android {random.randint(9,13)}; {"".join(random.choices(string.ascii_uppercase, k=3))}{random.randint(111,999)}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Mobile Safari/537.36'
-            res = requests.get("https://www.facebook.com/",headers={'user-agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'},proxies=proxies, timeout=30)
-            js_datr = res.text.split('["_js_datr","')[1].split('",')[0]
-            r=requests.get('https://www.instagram.com/api/v1/web/accounts/login/ajax/',headers={
+            r=requests.get('https://www.instagram.com/',headers={
                 'user-agent': an_agent
-            },proxies=proxies,timeout=30).cookies
-
+            },proxies=proxies,timeout=30)
+            js_datr = r.cookies['datr']
+            mid=r.text.split('{"mid":{"value":"')[1].split('",')[0]
             headers1 = {
                 'authority': 'www.instagram.com',
                 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
                 'accept-language': f'{Language}-{Country},en-GB;q=0.9,en-US;q=0.8,en;q=0.7',
-                'cookie': f'dpr=3; csrftoken={r["csrftoken"]}; mid={r["mid"]}; ig_nrcb=1; ig_did={r["ig_did"]}; datr={js_datr}',
+                'cookie': f'dpr=3; csrftoken={r.cookies["csrftoken"]}; mid={mid}; ig_nrcb=1; ig_did={r.cookies["ig_did"]}; datr={js_datr}',
                 'sec-ch-prefers-color-scheme': 'light',
                 'sec-ch-ua': '"Chromium";v="111", "Not(A:Brand";v="8"',
                 'sec-ch-ua-mobile': '?1',
@@ -45,7 +43,7 @@ def get_headers(Country,Language):
                 'accept': '*/*',
                 'accept-language': f'{Language}-{Country},en-GB;q=0.9,en-US;q=0.8,en;q=0.7',
                 'content-type': 'application/x-www-form-urlencoded',
-                'cookie': f'dpr=3; csrftoken={r["csrftoken"]}; mid={r["mid"]}; ig_nrcb=1; ig_did={r["ig_did"]}; datr={js_datr}',
+                'cookie': f'dpr=3; csrftoken={r.cookies["csrftoken"]}; mid={mid}; ig_nrcb=1; ig_did={r.cookies["ig_did"]}; datr={js_datr}',
                 'origin': 'https://www.instagram.com',
                 'referer': 'https://www.instagram.com/accounts/signup/email/',
                 'sec-ch-prefers-color-scheme': 'light',
@@ -58,12 +56,12 @@ def get_headers(Country,Language):
                 'user-agent': an_agent,
                 'viewport-width': '360',
                 'x-asbd-id': '198387',
-                'x-csrftoken': r["csrftoken"],
+                'x-csrftoken': r.cookies["csrftoken"],
                 'x-ig-app-id': str(appid),
                 'x-ig-www-claim': '0',
                 'x-instagram-ajax': str(rollout),
                 'x-requested-with': 'XMLHttpRequest',
-                'x-web-device-id': r["ig_did"],
+                'x-web-device-id': r.cookies["ig_did"],
             }
             return headers
         except Exception as E:
@@ -71,17 +69,13 @@ def get_headers(Country,Language):
 
 def Get_UserName(Headers,Name,Email):
     try:
-
         updict = {"referer": 'https://www.instagram.com/accounts/signup/birthday/'}
         Headers = {key: updict.get(key, Headers[key]) for key in Headers}
         while True:
-
-
             data = {
                 'email': Email,
                 'name': Name+str(random.randint(1,99)),
             }
-
             response = requests.post(
                 'https://www.instagram.com/api/v1/web/accounts/username_suggestions/',
                 headers=Headers,
@@ -99,14 +93,12 @@ def Get_UserName(Headers,Name,Email):
     except Exception as E:
         print(E)
 
-
 def Send_SMS(Headers,Email):
     try:
         data = {
             'device_id': Headers['cookie'].split('mid=')[1].split(';')[0],
             'email': Email,
 }
-
         response = requests.post(
             'https://www.instagram.com/api/v1/accounts/send_verify_email/',
             headers=Headers,
@@ -119,21 +111,15 @@ def Send_SMS(Headers,Email):
         print(E)
 
 
-
 def Validate_Code(Headers,Email,Code):
-
     try:
         updict = {"referer": 'https://www.instagram.com/accounts/signup/emailConfirmation/'}
         Headers = {key: updict.get(key, Headers[key]) for key in Headers}
-
-
-
         data = {
             'code': Code,
             'device_id': Headers['cookie'].split('mid=')[1].split(';')[0],
             'email': Email,
         }
-
         response = requests.post(
             'https://www.instagram.com/api/v1/accounts/check_confirmation_code/',
             headers=Headers,
@@ -143,12 +129,8 @@ def Validate_Code(Headers,Email,Code):
         )
         return response
 
-
-
     except Exception as E:
         print(E)
-
-
 
 def Create_Acc(Headers,Email,SignUpCode):
 
@@ -156,11 +138,8 @@ def Create_Acc(Headers,Email,SignUpCode):
         firstname=names.get_first_name()
         UserName=Get_UserName(headers,firstname,Email)
         Password=firstname.strip()+'@'+str(random.randint(111,999))
-
         updict = {"referer": 'https://www.instagram.com/accounts/signup/username/'}
         Headers = {key: updict.get(key, Headers[key]) for key in Headers}
-
-
         data = {
             'enc_password': f'#PWD_INSTAGRAM_BROWSER:0:{round(time.time())}:{Password}',
             'email': Email,
@@ -175,7 +154,6 @@ def Create_Acc(Headers,Email,SignUpCode):
             'force_sign_up_code': SignUpCode,
         }
         print(data)
-
         response = requests.post(
             'https://www.instagram.com/api/v1/web/accounts/web_create_ajax/',
             headers=Headers,
@@ -205,6 +183,7 @@ if __name__ == "__main__":
     print('Tool Made By @NamasteHacker')
 
     headers=get_headers(Country='US',Language='en')   # Enter Your Country With Your Local Language In Short Code
+
     Email=input('Enter Your Email : ')       # Enter Your Email
     ss=Send_SMS(headers,Email)
     print(ss)
@@ -218,6 +197,5 @@ if __name__ == "__main__":
         else:
             print(a.text)
     else :pass
-
 
 
